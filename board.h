4716c460effa10e5c365717730d73d05fda4b8f9
williamrhee <williamrhee@snu.ac.kr>
2023-04-17 23:48:12 +0900

    Included all the files in the zip file (Ignore all previous commits)

--- board.h
+++ board.h
@@ -1,7 +1,11 @@
 #include <fstream>
+
+#include "page.h"
+
+using std::endl;
 using std::ofstream;
 
-class Board{
+class Board {
     public:
         Board(int num_jobs, int width, int height, ofstream& output_stream);
         ~Board();
@@ -9,14 +13,89 @@ class Board{
         void print_board();
         void print_job(int job_idx, char job_type, int id);
 
-        // job functions
+        //job functions
         void insert_page(int x, int y, int width, int height, int id, int content);
         void delete_page(int id);
-        void modify_content(int id, char content); 
+        void modify_content(int id, char content);
         void modify_position(int id, int x, int y);
 
     private:
-        int num_jobs, width, height;
-        ofstream& output;
-        char* board;
+        int num_jobs, width, height; 
+        ofstream& output; 
+        char* board; 
 };
+
+
+Board::Board(int num_jobs, int width, int height, ofstream& output_stream): output(output_stream) {
+    this->width = width;
+    this->height = height;
+    this->num_jobs = num_jobs;
+
+    board = new char[width*height];
+
+    for (int h = 0; h < height; h++) {
+        for (int w = 0; w < width; w++) {
+            board[h*width + w] = ' ';
+        }
+    }
+
+}
+
+Board::~Board() {
+    delete board;
+    
+}
+
+
+void Board::print_board() {
+    int h, w;
+    for (w = 0; w < width+2; w++) output << "- ";
+    output << endl;
+    
+    for (h = 0; h < height; h++) {
+        output << "| ";
+        for (w = 0; w < width; w++) {
+            output << board[h*width + w] << " ";
+        }
+        output << "| " << endl;
+    }
+
+    for (w = 0; w < width+2; w++) output << "- ";
+    output << endl;
+}
+
+void Board::print_job(int job_idx, char job_type, int id) {
+    output << ">> (" << job_idx <<") ";
+    switch(job_type) {
+        
+        case 'i':
+            output << "Insert ";
+            break;
+        case 'd':
+            output << "Delete ";
+            break;
+        case 'm':
+            output << "Modify ";
+            break;
+    }
+
+    output << id << endl;
+}
+
+
+void Board::insert_page(int x, int y, int width, int height, int id, int content) {
+
+}
+
+void Board::delete_page(int id) {
+    
+}
+
+void Board::modify_content(int id, char content) {
+   
+
+}
+void Board::modify_position(int id, int x, int y) {
+   
+    
+}
--- page.h
+++ page.h
@@ -1,9 +1,9 @@
-class Page{
+class Page {
     public:
 
     private:
-        int x,y; //position of the page on the board
-        int width, height; // width and height of the page
+        int x, y; // position of the page on the board
+        int width, height; // width and height of the page 
         int id; // unique id for each page
-        char content; // content to be added to the board array
+        char content; 
 };
