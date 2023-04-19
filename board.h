bb95497bfde5053431fdfd1d0de1399554342022
williamrhee <williamrhee@snu.ac.kr>
2023-04-19 22:38:49 +0900

    Fixed Error with delete_page (delete_page not fully showing the steps)

--- board.h
+++ board.h
@@ -105,18 +105,28 @@ void Board::insert_page_again(int x, int y, int width, int height, int id, char 
             board[h*this->width+w] = content;
         }
     }
-    this->print_board();
 }
 
 void Board::delete_page(int id) {
     int i = vectorPages.size();
 
-    // Delete pages above
+    // Delete pages above and rewrite one by one
     while(vectorPages[i-1].getPageid() != id) {
         for (int h = vectorPages[i-1].gety(); h < vectorPages[i-1].gety() + vectorPages[i-1].getheight(); h++) {
             for (int w = vectorPages[i-1].getx(); w < vectorPages[i-1].getx() + vectorPages[i-1].getwidth(); w++) {
                 board[h*this->width+w] = ' ';
             }
+        }
+
+    // Rewrite the remaining
+        for(int k = 0; k < i-1 ; k++) {
+            int x = vectorPages[k].getx();
+            int y = vectorPages[k].gety();
+            int width = vectorPages[k].getwidth();
+            int height = vectorPages[k].getheight();
+            int id = vectorPages[k].getPageid();
+            char content = vectorPages[k].getcontent();
+            insert_page_again(x,y,width,height,id,content);
         }
         this->print_board();
         i--;
@@ -128,6 +138,7 @@ void Board::delete_page(int id) {
             board[h*this->width+w] = ' ';
         }
     }
+
     this->print_board();
 
     // Restore the pages above
@@ -140,6 +151,7 @@ void Board::delete_page(int id) {
         char content = vectorPages[i].getcontent();
         insert_page_again(x,y,width,height,id,content);
     }
+    this->print_board();
 }
 
 void Board::modify_content(int id, char content) {
